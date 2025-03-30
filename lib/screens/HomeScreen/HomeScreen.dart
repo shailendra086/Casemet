3c843f5609c11fb8a3928064993a5aa4d9cdb665
casemetsolutionspvtltd <casemetsolutionsprivatelimited@gmail.com>
2025-03-30 09:20:29 +0530

    'XML'

---
+++ lib/screens/HomeScreen/HomeScreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to the Home Screen!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to another screen or perform an action
+              },
+              child: const Text('Go to Details'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
